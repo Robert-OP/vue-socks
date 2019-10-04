a21b3a39b07b2094e55bdfdacca65bb9b975599d
Robert-OP <popescu.robert.octavian@gmail.com>
2019-10-04 19:29:25 +0200

    add variants, methods, buttons and switch between images

--- main.js
+++ main.js
@@ -3,7 +3,34 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'I love socks.',
-    image: './vmSocks-green-onWhite.jpg'
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    inventory: 12,
+    onSale: true,
+    image: './assets/vmSocks-green-onWhite.jpg',
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: './assets/vmSocks-green-onWhite.jpg'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: './assets/vmSocks-blue-onWhite.jpg'
+      }
+    ],
+    size: ['37', '39', '40', '41', '42', '43'],
+    cart: 0
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1;
+    },
+    removeFromCart: function() {
+      this.cart > 0 ? (this.cart -= 1) : null;
+    },
+    updateProduct: function(variantImage) {
+      this.image = variantImage;
+    }
   }
 });
