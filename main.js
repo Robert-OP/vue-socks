b867efab8bf010f09fc56bb834239f26393ed918
Robert-OP <popescu.robert.octavian@gmail.com>
2019-10-04 18:56:34 +0200

    first socks

---
+++ main.js
@@ -0,0 +1,9 @@
+// heart of app - plugs into an element of the DOM
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'I love socks.',
+    image: './vmSocks-green-onWhite.jpg'
+  }
+});
